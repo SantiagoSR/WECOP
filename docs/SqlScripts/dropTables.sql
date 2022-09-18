ad2fcc8451e71e92ede2a56f1f5a33aa2f08fc32
SamtiagoSR <samtiago.santa.ramirez@gmail.com>
2022-09-18 12:16:37 -0500

    edit drop tables.sql

--- docs/SqlScripts/dropTables.sql
+++ docs/SqlScripts/dropTables.sql
@@ -1,4 +1,6 @@
-DROP TABLE IF EXISTS `reviews`;
-DROP TABLE IF EXISTS `users`;
-DROP TABLE IF EXISTS `eco_products`;
-DROP TABLE IF EXISTS `not_eco_products`;
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS laravel.reviews;
+DROP TABLE IF EXISTS laravel.users;
+DROP TABLE IF EXISTS laravel.eco_products;
+DROP TABLE IF EXISTS laravel.not_eco_products;
+SET foreign_key_checks = 1;
