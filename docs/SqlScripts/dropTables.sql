c87b5161e91bd0b37a0ed0727d524200fa3f88e0
SamtiagoSR <samtiago.santa.ramirez@gmail.com>
2022-09-18 16:43:15 -0500

    pass the .sql insert, tryint to see what is on eco products to verify V7

--- docs/SqlScripts/dropTables.sql
+++ docs/SqlScripts/dropTables.sql
@@ -1,6 +1,7 @@
-SET foreign_key_checks = 0;
-DROP TABLE IF EXISTS laravel.reviews;
-DROP TABLE IF EXISTS laravel.users;
-DROP TABLE IF EXISTS laravel.eco_products;
-DROP TABLE IF EXISTS laravel.not_eco_products;
-SET foreign_key_checks = 1;
+-- SET foreign_key_checks = 0;
+-- DROP TABLE IF EXISTS laravel.reviews;
+-- DROP TABLE IF EXISTS laravel.users;
+-- DROP TABLE IF EXISTS laravel.eco_products;
+-- DROP TABLE IF EXISTS laravel.not_eco_products;
+-- SET foreign_key_checks = 1;
+SELECT * FROM `laravel`.eco_products;
